7a8ebd1291d2f82c94585fae9aeea63fe8d8296f
Arhaan <113068699+arhaankk@users.noreply.github.com>
2025-10-05 15:49:29 -0400

    added dynamic selection

--- main.py
+++ main.py
@@ -15,42 +15,58 @@ def main():
         "mr": "Sanjay"
     }
 
-    input_text = "kal mai riya se baat kar rha tha, aur fir usne bola ki, chal breakup kar le abhi. fir maine usko 50000 bheja sephora ke liye, fir kush ho gayi."  
+    transliterator_list = {
+        "hi": HindiTransliterator(),
+        "te": TeluguTransliterator()
+    }
+
+    input_text = "Indha naala weather romba nalla irukku."  
     
     output_filename = "output.wav"
 
-    # Step 1: Transliteration.
-    transliterator = HindiTransliterator()
-    transliterator.run_pipeline()
-    transliterated_text = transliterator.transliterate(input_text)
-    print("Transliterated text:", transliterated_text)
+    # Step 1: Classification
+    try:
+        classifier = LanguageClassifier(file_paths, languages)
+        classifier.load_data()
+        x = classifier.prepare_tokenizer()
+        y = classifier.encode_labels()
 
-    # Step 2: Classification.
-    classifier = LanguageClassifier(file_paths, languages)
-    classifier.load_data()
-    x = classifier.prepare_tokenizer()
-    y = classifier.encode_labels()
+        classifier.load_or_train_model(x, y)
 
-
-    classifier.load_or_train_model(x, y)
-
-    classification_result = classifier.predict_language(input_text)
-    print("Classification result:", classification_result)
+        classification_result = classifier.predict_language(input_text)
+        print("Classification result:", classification_result)
+    except:
+        print("Language not supported yet")
+        
 
 
-    speaker = voice_list.get(classification_result)
+    # Step 2: Transliteration.
+    try:
+        transliterator = transliterator_list.get(classification_result)
+        transliterator.run_pipeline()
+        transliterated_text = transliterator.transliterate(input_text)
+        print("Transliterated text:", transliterated_text)
+    except:
+        print("No available transliterator. Please check classification.")
+
+        
+
     
-    description = f"{speaker}'s voice is clear and friendly with a moderate pace."
-    print(f"SPEAKER IS {speaker}")
     # Step 3: Speech synthesis.
-    combined_description = f"{description} Classified as: {classification_result}"
-    tts_generator = SpeechGenerator()
-    tts_generator.generate_speech(
-        prompt=transliterated_text,
-        description=combined_description,
-        output_filename=output_filename
-    )
-    print(f"Audio generated and saved as {output_filename}")
+    try:
+        speaker = voice_list.get(classification_result)
+        description = f"{speaker}'s voice is clear and friendly with a moderate pace."
+        print(f"SPEAKER IS {speaker}")
+        combined_description = f"{description} Classified as: {classification_result}"
+        tts_generator = SpeechGenerator()
+        tts_generator.generate_speech(
+            prompt=transliterated_text,
+            description=combined_description,
+            output_filename=output_filename
+        )
+        print(f"Audio generated and saved as {output_filename}")
+    except:
+        print("No available TTS. Please check classification.")
 
 if __name__ == "__main__":
     main()
